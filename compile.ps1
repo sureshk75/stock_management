0909d49b41aab25cf3f67d3756c49451ea72dd19
Suresh Kumar <sureshk75@gmail.com>
2024-03-18 21:09:12 +0800

    v0.0.1

---
+++ compile.ps1
@@ -0,0 +1,6 @@
+﻿$to_del = "__pycache__", "build", "dist", "stck_invt.spec"
+foreach ($item in $to_del)
+{
+    Remove-Item $item -Force -Recurse -ErrorAction SiletlyContinue
+}
+pyinstaller --onefile --windowed --add-data "resource/*;resource" --name "stck_invt" --icon=resource/icon.ico --version-file="file_version_info.txt" controller.py
